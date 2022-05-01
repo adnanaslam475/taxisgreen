8e9f40ed2384fab52232b7b8c7416b7356e98a2d
adnan aslam <adnanaslam475@gmail.com>
2022-05-01 22:25:58 +0500

    awewee

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+const path = require('path');
+const pak = require('../package.json');
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
+};
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {
+          [pak.name]: path.join(__dirname, '..', pak.source),
+        },
+      },
+    ],
+    'react-native-reanimated/plugin'
+  ]
 };
